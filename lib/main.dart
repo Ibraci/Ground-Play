866cdacfe7259766d75aa9fcead3f6aae99dc473
ibraci <ibraci1960@gmail.com>
2025-06-19 14:24:05 +0000

    Add widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,36 @@
 import 'package:flutter/material.dart';
-import './forms.dart';
+import 'package:ground_play/data/notifiers.dart';
+import 'pages/home_page.dart';
 
-void main() => runApp(FormExample());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: isDarkNotif,
+      builder: (context, isDarkMode, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: isDarkMode ? ThemeData.dark(
+            primarySwatch: Color: 
+          ) : ThemeData.light(),
+          home: Scaffold(
+            appBar: AppBar(
+              title: Text('Playground'),
+              actions: [
+                IconButton(
+                  onPressed: () => {isDarkNotif.value = !isDarkNotif.value},
+                  icon: Icon(isDarkMode ? Icons.light_mode : Icons.dark_mode),
+                ),
+              ],
+            ),
+            body: HomePage(),
+          ),
+        );
+      },
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ground_play/caroussel_slider.dart';
+import 'package:ground_play/widgets/caroussel_slider.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
